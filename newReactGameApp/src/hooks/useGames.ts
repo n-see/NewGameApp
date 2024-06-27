d3b39ac11ea3afd5959ebe040fc73ede49980d14
Neo See <neococo123@yahoo.com>
2024-06-26 20:32:42 -0700

    fixed one letter

--- newReactGameApp/src/hooks/useGames.ts
+++ newReactGameApp/src/hooks/useGames.ts
@@ -20,6 +20,6 @@ export interface FetchGameResponse {
 
 
 
-const useGames = (selectedGenre:Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games',{params:{genres:selectedGenre?.id, parent_platform:selectedPlatform?.id}},[selectedGenre?.id, selectedPlatform?.id])
+const useGames = (selectedGenre:Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games',{params:{genres:selectedGenre?.id, parent_platforms:selectedPlatform?.id}},[selectedGenre?.id, selectedPlatform?.id])
 
 export default useGames;
